1591baba7351a1f43ef81a30022ba4044dbe8338
rohit kavathekar <rohityk.2024@gmail.com>
2026-02-09 23:24:49 +0530

    solved

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/projects/:id" element={<ProjectDetails />} />
+
         <Route path="/contact" element={<Contact />} />
       </Routes>
 
